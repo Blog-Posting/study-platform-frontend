008f58e3b06c50ed6ae45fcc8a4fbcac28610afe
Duckhee Lee <shldhee@yahoo.co.kr>
2021-02-01 07:51:08 +0900

    chore: set TailWindCSS

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// postcss.config.js
+
+module.exports = {
+  plugins: [
+    // ...
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // ...
+  ],
+};
