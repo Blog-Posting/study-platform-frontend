788ae5460557c8b78e0b64bf2012e4078478efa0
Duckhee Lee <shldhee@yahoo.co.kr>
2021-02-11 18:37:51 +0900

    [#2, #11] feat: 프로젝트 세팅 및 헤더 작업 (#12)
    * chore: set TailWindCSS
    
    * feat: set Layouts

---
+++ src/components/Layouts/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="nav w-48 bg-gray-500 text-white p-4">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
