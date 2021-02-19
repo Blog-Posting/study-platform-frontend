67855d81c7bbc26d12a9d63ce5f6c2a418044de8
Duckhee Lee <shldhee@yahoo.co.kr>
2021-02-19 13:26:44 +0900

    feat: FeedCard 컴포넌트 생성

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="home">home main</div>
+  <div class="home">
+    <h2>home title</h2>
+    <div class="content">
+      <FeedCard />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { FeedCard } from '@/components/Cards';
 
 export default defineComponent({
+  name: 'Home',
+  components: { FeedCard },
   setup() {
     return {};
   },
