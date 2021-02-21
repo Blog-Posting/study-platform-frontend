d50b0d5e90822c400a60681a542a06f6b83d2581
ksy90101 <ksy90101@gmail.com>
2021-02-21 16:36:49 +0900

    feat: Feed Card 제작

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="home">
     <h2>home title</h2>
-    <div class="content">
-      <FeedCard />
+    <div class="container my-12 mx-auto px-4 md:px-12">
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+      </div>
     </div>
   </div>
 </template>
