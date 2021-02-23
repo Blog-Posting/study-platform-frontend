d23ad91960003f28c2e9222a4b690dc731833229
Duckhee Lee <shldhee@yahoo.co.kr>
2021-02-23 13:13:46 +0900

    feat: feedcard 레이아웃 수정

--- src/components/Cards/FeedCard.vue
+++ src/components/Cards/FeedCard.vue
@@ -1,43 +1,47 @@
 <template>
-  <article class="overflow-hidden rounded-lg shadow-lg my-2.5">
-    <a href="#">
-      <img alt="Placeholder" class="block h-auto w-full" :src="img" />
-    </a>
-
-    <header class="leading-tight p-2 md:p-4">
-      <h1 class="text-lg">
-        <a class="no-underline hover:underline text-black" href="#"> {{ title }} </a>
-      </h1>
-    </header>
-
-    <section>
-      <p class="text-grey-darker text-sm">{{ description }}</p>
-    </section>
-
-    <hr class="my-2" />
-
-    <footer class="flex items-center justify-between leading-none p-1 md:p-2">
-      <a class="flex items-center no-underline hover:underline text-black" href="#">
-        <img
-          alt="Placeholder"
-          class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-          :src="userProfile"
-        />
-        <p class="ml-2 text-sm">{{ name }}</p>
+  <article class="w-full md:w-1/2 xl:w-1/3 flex flex-col p-3">
+    <div class="overflow-hidden rounded-lg shadow-lg my-2.5">
+      <a href="#">
+        <img alt="Placeholder" class="block h-auto w-full" :src="img" />
       </a>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        width="24px"
-        height="24px"
-      >
-        <path
-          d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0
+
+      <header class="leading-tight p-2 md:p-4">
+        <h1 class="text-lg">
+          <a class="no-underline hover:underline text-black" href="#">
+            {{ title }}
+          </a>
+        </h1>
+      </header>
+
+      <section>
+        <p class="text-grey-darker text-sm">{{ description }}</p>
+      </section>
+
+      <hr class="my-2" />
+
+      <footer class="flex items-center justify-between leading-none p-1 md:p-2">
+        <a class="flex items-center no-underline hover:underline text-black" href="#">
+          <img
+            alt="Placeholder"
+            class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
+            :src="userProfile"
+          />
+          <p class="ml-2 text-sm">{{ name }}</p>
+        </a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          width="24px"
+          height="24px"
+        >
+          <path
+            d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0
             010 4z"
-        />
-      </svg>
-    </footer>
+          />
+        </svg>
+      </footer>
+    </div>
   </article>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <h2>home title</h2>
-    <div class="container my-12 mx-auto px-4 md:px-12">
-      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+    <div class="container flex flex-wrap my-12 mx-auto px-4 md:px-12">
+      <FeedCard v-for="n in 9" :key="n" />
+      <!-- <div class="flex flex-wrap -mx-1 lg:-mx-4">
         <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
           <FeedCard />
           <FeedCard />
@@ -18,7 +19,7 @@
           <FeedCard />
           <FeedCard />
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
