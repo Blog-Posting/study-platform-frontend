8c97a72da8ecc89928ed853331b15a812bd3015b
Duckhee Lee <shldhee@yahoo.co.kr>
2021-03-16 11:56:05 +0900

    feat: infinite scroll 구현 (#21)
    * chore: set TailWindCSS
    
    * feat: set Layouts
    
    * fix: fix eslint rule
    
    * feat: infinite scroll 적용

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,58 @@
   <div class="home">
     <h2>home title</h2>
     <div class="container flex flex-wrap my-12 mx-auto px-4 md:px-12">
-      <FeedCard v-for="n in 9" :key="n" />
-      <!-- <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-        </div>
-        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-        </div>
-        <div class="my-2 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-        </div>
-      </div> -->
+      <FeedCard v-for="n in list" :key="n" />
     </div>
+    <div class="end"></div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs, onMounted } from 'vue';
 import { FeedCard } from '@/components/Cards';
 
 export default defineComponent({
   name: 'Home',
   components: { FeedCard },
   setup() {
-    return {};
+    const feed = reactive({
+      list: 9,
+      // computedList: computed(() => {
+      //   return
+      // })
+    });
+    function fetchItems() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          feed.list += 9;
+          resolve('done');
+        }, 3000);
+      });
+    }
+    onMounted(() => {
+      const options = {
+        root: null,
+        rootMargin: '0px 0px 30px 0px',
+        threshold: 0,
+      };
+
+      const io = new IntersectionObserver(async ([entry], observer) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          // observer.disconnect();
+          console.log('fetchItems()', fetchItems());
+          try {
+            await fetchItems();
+          } catch (error) {
+            console.error(error);
+          }
+          observer.observe(entry.target);
+        }
+      }, options);
+
+      const el = document.querySelector('.end') as HTMLElement;
+      io.observe(el);
+    });
+    return { ...toRefs(feed), fetchItems };
   },
 });
 </script>
